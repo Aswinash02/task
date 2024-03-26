f0628fa6ba5abde0fd3fc5cae428b0c06630b7e1
Aswinash02 <160094201+Aswinash02@users.noreply.github.com>
2024-03-25 22:24:28 -0800

    Add files via upload

---
+++ lib/common/constant/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color scaffoldColor = Colors.grey;
+  static const Color blueAccentColor = Colors.blueAccent;
+  static const Color whiteColor = Colors.white;
+  static const Color black38Color = Colors.black38;
+  static const Color greyColor = Colors.grey;
+  static Color greyShade = Colors.grey.shade200;
+  static Color indigoShade = Colors.indigo.shade100;
+  static const Color indigoColor = Colors.indigo;
+}
