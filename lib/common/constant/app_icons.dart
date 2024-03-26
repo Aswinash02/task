99e7660bf58c7f8ae9c2e51a360a371920a260c2
Aswinash02 <160094201+Aswinash02@users.noreply.github.com>
2024-03-25 20:21:37 -0800

    Add files via upload
    ui task

---
+++ lib/common/constant/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppIcons{
+  static const IconData arrowDownIcon=Icons.keyboard_arrow_down;
+  static const IconData personIcon=Icons.person;
+  static const IconData notificationIcon=Icons.notifications;
+  static const IconData arrowBackIcon=Icons.arrow_back;
+  static const IconData arrowForwardIcon=Icons.arrow_forward;
+  static const IconData fullscreenIcon=Icons.fullscreen;
+  static const IconData searchIcon=Icons.search;
+}
